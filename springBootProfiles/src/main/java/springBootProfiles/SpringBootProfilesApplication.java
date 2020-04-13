db7bc53973110f8c945f8fa40c9234d2691f4d81
Thamaraikannan <rtkcse@hotmail.com>
2020-04-13 22:53:20 +0530

    profiles for application properties and bean based

---
+++ springBootProfiles/src/main/java/springBootProfiles/SpringBootProfilesApplication.java
@@ -0,0 +1,13 @@
+package springBootProfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProfilesApplication.class, args);
+	}
+
+}
---
+++ springBootProfiles/src/test/java/springBootProfiles/SpringBootProfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package springBootProfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProfilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
