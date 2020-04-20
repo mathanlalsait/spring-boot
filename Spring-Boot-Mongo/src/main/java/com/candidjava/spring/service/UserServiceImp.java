796e941dd889fc1acdf977d36e8b77ef88b593c4
Thamaraikannan <rtkcse@hotmail.com>
2020-04-20 19:23:23 +0530

    no sql mongoDb example

--- Spring-Boot-Mongo/src/main/java/com/candidjava/spring/service/UserServiceImp.java
+++ Spring-Boot-Mongo/src/main/java/com/candidjava/spring/service/UserServiceImp.java
@@ -1,6 +1,7 @@
 package com.candidjava.spring.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,9 +26,9 @@ public class UserServiceImp implements UserService {
 		return (List<User>) userRepository.findAll();
 	}
 
-	public User findById(String id) {
+	public Optional<User> findById(String id) {
 		// TODO Auto-generated method stub
-		return userRepository.findOne(id);
+		return userRepository.findById(id);
 	}
 
 	public User update(User user, String l) {
@@ -37,14 +38,15 @@ public class UserServiceImp implements UserService {
 
 	public void deleteUserById(String id) {
 		// TODO Auto-generated method stub
-		userRepository.delete(id);
+		userRepository.deleteById(id);
 	}
 
 	public User updatePartially(User user, String id) {
 		// TODO Auto-generated method stub
-		User usr = findById(id);
-		usr.setCountry(user.getCountry());
-		return userRepository.save(usr);
+		Optional<User> usr = findById(id);
+		User singleUser = usr.get();
+		singleUser.setCountry(user.getCountry());
+		return userRepository.save(singleUser);
 	}
 
 
