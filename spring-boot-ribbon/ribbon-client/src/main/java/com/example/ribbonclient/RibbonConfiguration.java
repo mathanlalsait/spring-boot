36b215270cd6ecc73cb7dd8a9c71508b345eda5c
Thamaraikannan <rtkcse@hotmail.com>
2020-05-26 12:40:22 +0530

    spring cloud ribbon concept completed

---
+++ spring-boot-ribbon/ribbon-client/src/main/java/com/example/ribbonclient/RibbonConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.ribbonclient;
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.AvailabilityFilteringRule;
+import com.netflix.loadbalancer.IPing;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.PingUrl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class RibbonConfiguration {
+
+    @Autowired
+    IClientConfig config;
+
+    @Bean
+    public IPing ribbonPing(IClientConfig config) {
+        return new PingUrl();
+    }
+
+    @Bean
+    public IRule ribbonRule(IClientConfig config) {
+        return new AvailabilityFilteringRule();
+    }
+}
