42f5f343a3f1cd3c9591f15ebe9e946a507d2340
Thamaraikannan <rtkcse@hotmail.com>
2020-05-27 23:20:03 +0530

    spring cloud zuul example completed

---
+++ spring-cloud-zuul/Spring-Boot-RestCrud/src/main/java/com/candidjava/springboot/SpringCrudApplication.java
@@ -0,0 +1,14 @@
+
+package com.candidjava.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-zuul/zuul-api-gateway/src/main/java/com/candidjava/ZuulApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.candidjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApiGatewayApplication.class, args);
+	}
+
+}
