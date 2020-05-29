20ebb3b1cb610c470f916857bd2201c8d4e8a9d1
Thamaraikannan <rtkcse@hotmail.com>
2020-05-29 12:20:57 +0530

    zuul with eureka completed

---
+++ spring-boot-zuul-with-eureka/zuul-eureka-server/src/test/java/com/example/ribboneurekaserver/RibbonEurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.ribboneurekaserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RibbonEurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
