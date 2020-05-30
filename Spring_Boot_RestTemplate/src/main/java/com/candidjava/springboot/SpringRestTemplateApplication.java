984127b1a3b81000338f43adad472bf1e1328908
Dinesh <dineshk3094@gmail.com>
2020-05-30 23:45:07 +0530

    resttemplate_builder

--- Spring_Boot_RestTemplate/src/main/java/com/candidjava/springboot/SpringRestTemplateApplication.java
+++ Spring_Boot_RestTemplate/src/main/java/com/candidjava/springboot/SpringRestTemplateApplication.java
@@ -3,6 +3,7 @@ package com.candidjava.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,8 +11,8 @@ import org.springframework.web.client.RestTemplate;
 public class SpringRestTemplateApplication {
 
 	@Bean
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 
 	public static void main(String[] args) {
