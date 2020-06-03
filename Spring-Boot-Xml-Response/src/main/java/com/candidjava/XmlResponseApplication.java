46b8d071b1e108603a0a988f9585e20e30eab026
Dinesh <dineshk3094@gmail.com>
2020-06-03 12:59:17 +0530

    Spring_Boot_Xml_Response

---
+++ Spring-Boot-Xml-Response/src/main/java/com/candidjava/XmlResponseApplication.java
@@ -0,0 +1,13 @@
+package com.candidjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlResponseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlResponseApplication.class, args);
+	}
+
+}
